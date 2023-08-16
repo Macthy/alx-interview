15e0b442ffdc83e1672b8ef1d199a9a5e69e8a16
thymac <thywillberdone@gmail.com>
2023-08-16 11:49:23 +0000

    revised

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,30 +1,61 @@
+#!/usr/bin/env python3
+
+"""
+Lockboxes Problem:
+You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes. Write a method that determines if all the boxes can be opened.
+
+Prototype: def canUnlockAll(boxes)
+boxes is a list of lists
+A key with the same number as a box opens that box
+You can assume all keys will be positive integers
+There can be keys that do not have boxes
+The first box boxes[0] is unlocked
+Return True if all boxes can be opened, else return False
+"""
+
 def canUnlockAll(boxes):
     """
-    Check if all the boxes can be unlocked, given the list of boxes and their keys.
-
-    Args:
-        boxes (list of lists): A list of lists where each inner list represents a box
-                               and contains keys to other boxes.
+    Determine if all boxes can be unlocked.
+    
+    Parameters:
+        boxes (list): A list of lists representing boxes and their keys.
 
     Returns:
-        bool: True if all the boxes can be unlocked, False otherwise.
+        bool: True if all boxes can be unlocked, False otherwise.
     """
-    def dfs(box_idx):
-        """
-        Perform a depth-first search (DFS) to explore the boxes and unlock them.
+    from copy import deepcopy
 
-        Args:
-            box_idx (int): The index of the current box being visited.
+    if type(boxes) is not list or len(boxes) < 1:
+        return False
 
-        """
-        visited.add(box_idx)  # Mark the current box as visited
-        for key in boxes[box_idx]:  # Iterate through the keys in the current box
-            if key not in visited:  # Check if the key points to an unvisited box
-                dfs(key)  # Recursively visit the box pointed by the key
+    for box in boxes:
+        if type(box) is not list:
+            return False
 
-    n = len(boxes)
-    visited = set()  # Create an empty set to keep track of visited boxes
-    dfs(0)  # Start DFS from the first box (box with index 0)
+    copyBoxes = deepcopy(boxes)
+    keys_list = [0]
 
-    return len(visited) == n  # Check if all boxes have been visited
+    while len(keys_list) > 0:
+        key = keys_list[0]
+        keys_list = keys_list[1:]
 
+        if type(key) is not int or key < 0:
+            return False
+
+        copyBoxes[key].append(-1)
+
+        for new_key in copyBoxes[key]:
+            if new_key is -1:
+                continue
+            if new_key >= len(copyBoxes):
+                continue
+            if -1 in copyBoxes[new_key] or new_key in keys_list:
+                continue
+            keys_list.append(new_key)
+
+    for box in copyBoxes:
+        if -1 not in box:
+            return False
+
+    return True
