49feecfb4b56c6e2007293b94f3bc99b018aae44
Macthy <thywilberdonemacharia@gmail.com>
2023-08-22 15:00:22 +0000

    revised

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
 """
-UTF-8 Validation module
+UTF-8 Validation
 """
 
-def validUTF8(data):
+
+def validUTF8(data) -> bool:
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data (list): A list of integers representing 1 byte of data each.
-
-    Returns:
-        bool: True if data is a valid UTF-8 encoding, else return False.
+    Returns True if data is a valid UTF-8 encoding, else return False
+    :param data:
+    :return:
     """
-    num_bytes_to_follow = 0
-
+    num_bytes = 0
     for byte in data:
-        if num_bytes_to_follow == 0:
-            if (byte >> 7) == 0b0:  # 1-byte character
-                num_bytes_to_follow = 0
-            elif (byte >> 5) == 0b110:  # 2-byte character
-                num_bytes_to_follow = 1
-            elif (byte >> 4) == 0b1110:  # 3-byte character
-                num_bytes_to_follow = 2
-            elif (byte >> 3) == 0b11110:  # 4-byte character
-                num_bytes_to_follow = 3
-            else:
+        mask = 1 << 7
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if not num_bytes:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            if byte >> 6 != 0b10:
                 return False
-            num_bytes_to_follow -= 1
-
-    return num_bytes_to_follow == 0
-
+        num_bytes -= 1
+    return num_bytes == 0
